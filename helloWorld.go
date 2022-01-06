f3d0bd3749988109f3e18f7a4f1384edd892d994
Tongsang-creator <min35q1@gmail.com>
2022-01-06 22:33:31 +0700

    finish first program

--- helloWorld.go
+++ helloWorld.go
@@ -1,12 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello")
-	printFoo()
-}
-
-func printFoo() {
-	fmt.Println("Process in foo")
+	var input string
+	fmt.Scanf("%s", &input)
+	//fmt.Println(reflect.TypeOf(input))
+	intVar, _ := strconv.Atoi(input)
+	//fmt.Println(intVar)
+	//fmt.Println(intVar % 2)
+	if intVar > 2 && (intVar%2 == 0) {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
 }
