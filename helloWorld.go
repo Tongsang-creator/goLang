5e0b1af037911257395701f56ee198f3c39606c6
Tongsang-creator <min35q1@gmail.com>
2022-01-08 00:16:18 +0700

    delete comment

--- helloWorld.go
+++ helloWorld.go
@@ -8,10 +8,7 @@ import (
 func main() {
 	var input string
 	fmt.Scanf("%s", &input)
-	//fmt.Println(reflect.TypeOf(input))
 	intVar, _ := strconv.Atoi(input)
-	//fmt.Println(intVar)
-	//fmt.Println(intVar % 2)
 	if intVar > 2 && (intVar%2 == 0) {
 		fmt.Println("Yes")
 	} else {
