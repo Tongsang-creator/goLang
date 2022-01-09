bbc7a670c9a9a0df9c20d022a9b36881ec58b44b
Tongsang-creator <min35q1@gmail.com>
2022-01-09 15:38:28 +0700

    add const

--- helloWorld.go
+++ helloWorld.go
@@ -5,11 +5,16 @@ import (
 	"strconv"
 )
 
+const (
+	minNum    = 2
+	modulaNum = 2
+)
+
 func main() {
 	var input string
 	fmt.Scanf("%s", &input)
 	intVar, _ := strconv.Atoi(input)
-	if intVar > 2 && (intVar%2 == 0) {
+	if intVar > minNum && (intVar%modulaNum == 0) {
 		fmt.Println("Yes")
 	} else {
 		fmt.Println("No")
